2efecb64366e140bfab31d16c51bff6aaae4b704
unresolved <1760273210@qq.com>
2021-06-29 12:17:08 +0800

    创建基本项目结构

---
+++ src/main/java/com/totyu/totyudemo/TotyuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.totyu.totyudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotyuDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotyuDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/totyu/totyudemo/TotyudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totyu.totyudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotyudemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
